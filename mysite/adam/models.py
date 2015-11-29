8ea67e715cd81f0a3892c55d9689e0ff0bad60c6
Jesse Dovi <jdovi@drydenengineering.com>
2015-11-29 15:26:59 -0500

    Added new pages and views.  Still have a problem with path not being passed as a string.

--- mysite/adam/models.py
+++ mysite/adam/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class ADAMFiles(models.Model):
+    def __str__(self):
+        return self.DBFPath
+
+    DBFPath = models.FilePathField(
+        path='c:\winADAM\\',
+        recursive=True,
+    )
