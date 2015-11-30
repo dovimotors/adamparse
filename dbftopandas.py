4a12f9df3106b55d035eb161a31a0ce6facc73b6
jdovi <jesse@dovimotors.com>
2015-11-30 12:23:49 -0500

    added documentation to the code

--- dbftopandas.py
+++ dbftopandas.py
@@ -6,25 +6,33 @@ import numpy as np
 
 class AdamImport:
     def convert(self, filename):
+        #create the DBF reader object
         db = dbf.Dbf(filename)
+        #create the file to output CSV data to
         fpath = 'output.csv'
         f = open(fpath,'w')
+        #initiat the header container and CSV writer
         hdr = []
         c = csv.writer(f)
 
-
+        #loop through all the field names and create the header row
         for fieldName in db.fieldNames:
             hdr += [fieldName]
 
+        #write the head to the CSV file
         c.writerow(hdr)
 
+        #loop through all the records and write each line to the CSV file
         for rec in db:
+            #using asList makes a list as opposed to asDict
             c.writerow(rec.asList())
 
-
+        #take the csv file and open it as a pandas dataframe
         from_csv = pd.read_csv(fpath)
 
-
+        #close the CSV file
+        f.close()
+        #return a dataframe object
         return from_csv
 
 
--- mysite/adam/views.py
+++ mysite/adam/views.py
@@ -5,15 +5,28 @@ from .models import ADAMFiles
 
 # Create your views here.
 def index(request):
+    #main view for /adam/ app.  returns all paths in the database with links
     path_list = ADAMFiles.objects.all()
     context = {'path_list': path_list}
     return render(request, 'adam/index.html', context)
 
 def detail(request, path_id):
+    #view for the adam/<id> page returns an html rendered dataframe
+    #create the object to convert DBF to pandas
     ai = AdamImport()
+    #get the id from the path and pull the correspoding DBF
     p = ADAMFiles.objects.get(id=path_id)
     p = str(p)
-    inv = ai.convert(p)
-    s = inv.head()
+    #conver the DBF to a dataframe
+    datafr = ai.convert(p)
+
+    #*******************
+    # modify the dataframe here before converting to HTML
+    #*******************
+
+    s = datafr.head()
+
+    #*******************
+    #*******************
     s_trunk = s.to_html()
     return HttpResponse(s_trunk)
