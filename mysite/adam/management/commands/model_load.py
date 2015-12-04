7911f2e6e1c7f9186a1bf29cbb207db0d1692756
jdovi <jesse@dovimotors.com>
2015-12-04 16:57:39 -0500

    worked on load_models management command for dashboard

--- mysite/adam/management/commands/model_load.py
+++ mysite/adam/management/commands/model_load.py
@@ -1,9 +1,12 @@
 from django.core.management.base import BaseCommand
 from adam.models import ADAMFiles
+from dashboard.models import PartsInv
+import pandas as pd
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        """
         a = ADAMFiles.objects.all()
         print a
         for x in a:
@@ -16,3 +19,10 @@ class Command(BaseCommand):
         #c.save()
         #d = ADAMFiles.objects.get(id=3)
         #d.delete()
+        """
+
+        ifile = 'F:\\adamexports\csvfiles\INVEN.csv'
+        inv = pd.read_csv(ifile)
+        ext = inv.ONHAND * inv.COST
+        ext = sum(ext)
+        print ext
