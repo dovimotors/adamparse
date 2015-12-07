ea612e5fd7ad3a7e8b3f19fe7e3c37cd371d9b55
jdovi <jesse@dovimotors.com>
2015-12-07 16:54:55 -0500

    added service ro stats to the model_load script.  added /service page to the dashboard app.

--- mysite/dashboard/views.py
+++ mysite/dashboard/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, HttpResponse
-from .models import PartsInv
+from .models import PartsInv, ServiceRO
 import pandas as pd
 # Create your views here.
 
 
 def index(request):
     parts = PartsInv.objects.all()
-    context = {'value_list': parts}
+    service = ServiceRO.objects.all()
+    context = {'parts_list': parts,
+               'service_list':service,
+               }
     return render(request, 'dashboard/index.html', context)
 
-
+def service(request):
+    ro_data = pd.DataFrame(list(ServiceRO.objects.all().values()))
+    html = ro_data.to_html()
+    return HttpResponse(html)
